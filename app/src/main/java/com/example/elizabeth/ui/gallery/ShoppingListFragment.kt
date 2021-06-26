df57a47edd40fd90a6c6c3246c6700ec28e384db
Alexandr Omeluaniuk <alexandr.omeluaniuk@wisent-media.by>
2021-06-26 17:37:02 +0300

    Re-styling.

--- app/src/main/java/com/example/elizabeth/ui/gallery/ShoppingListFragment.kt
+++ app/src/main/java/com/example/elizabeth/ui/gallery/ShoppingListFragment.kt
@@ -8,12 +8,12 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.example.elizabeth.R
 import com.example.elizabeth.databinding.FragmentShoppingListsBinding
+import com.google.android.material.snackbar.Snackbar
 
-class GalleryFragment : Fragment() {
+class ShoppingListFragment : Fragment() {
 
-    private lateinit var galleryViewModel: GalleryViewModel
+    private lateinit var shoppingListViewModel: ShoppingListViewModel
     private var _binding: FragmentShoppingListsBinding? = null
 
     // This property is only valid between onCreateView and
@@ -25,16 +25,16 @@ class GalleryFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        galleryViewModel =
-            ViewModelProvider(this).get(GalleryViewModel::class.java)
+        shoppingListViewModel =
+            ViewModelProvider(this).get(ShoppingListViewModel::class.java)
 
         _binding = FragmentShoppingListsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textGallery
-        galleryViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+        binding.fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
         return root
     }
 
