05397574efd78cface4fa2aa6749f55ad0db6dbb
Alexandr Omeluaniuk <alexandr.omeluaniuk@wisent-media.by>
2021-06-26 17:25:43 +0300

    Re-styling.

--- app/src/main/java/com/example/elizabeth/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/example/elizabeth/ui/gallery/GalleryFragment.kt
@@ -9,12 +9,12 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.elizabeth.R
-import com.example.elizabeth.databinding.FragmentGalleryBinding
+import com.example.elizabeth.databinding.FragmentShoppingListsBinding
 
 class GalleryFragment : Fragment() {
 
     private lateinit var galleryViewModel: GalleryViewModel
-    private var _binding: FragmentGalleryBinding? = null
+    private var _binding: FragmentShoppingListsBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -28,7 +28,7 @@ class GalleryFragment : Fragment() {
         galleryViewModel =
             ViewModelProvider(this).get(GalleryViewModel::class.java)
 
-        _binding = FragmentGalleryBinding.inflate(inflater, container, false)
+        _binding = FragmentShoppingListsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textGallery
